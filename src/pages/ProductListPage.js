4f18bbec4b31b4729839ceef1c04d9743c291c2f
Mingeon Kim <mingeon.dev@gmail.com>
2020-04-17 08:47:47 +0900

    feat: Button 컴포넌트 추가

--- src/pages/ProductListPage.js
+++ src/pages/ProductListPage.js
@@ -1,7 +1,13 @@
 import React from 'react';
+import Button from '../components/common/Button';
 
 const ProductListPage = () => {
-  return <div>Product List</div>;
+  return (
+    <div>
+      Product List
+      <Button>확인</Button>
+    </div>
+  );
 };
 
 export default ProductListPage;
