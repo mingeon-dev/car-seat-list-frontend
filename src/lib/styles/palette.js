b9aa8d5152169c437e1f6119da8530bc3feab287
Mingeon Kim <mingeon.dev@gmail.com>
2020-04-20 08:34:00 +0900

    feat: palette에 gray 색상 추가

--- src/lib/styles/palette.js
+++ src/lib/styles/palette.js
@@ -1,5 +1,17 @@
 // source: https://yeun.github.io/open-color
 const palette = {
+  gray: [
+    '#f8f9fa',
+    '#f1f3f5',
+    '#e9ecef',
+    '#dee2e6',
+    '#ced4da',
+    '#adb5bd',
+    '#868e96',
+    '#495057',
+    '#343a40',
+    '#212529',
+  ],
   teal: [
     '#e6fcf5',
     '#c3fae8',
