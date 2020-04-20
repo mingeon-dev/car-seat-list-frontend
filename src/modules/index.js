9e16e5ce9348a2792c1c5774a365550492d811e3
Mingeon Kim <mingeon.dev@gmail.com>
2020-04-21 08:47:07 +0900

    feat: loading 리덕스 모듈 추가

--- src/modules/index.js
+++ src/modules/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import products from './products';
+import loading from './loading';
 
 const rootReducer = combineReducers({
   products,
+  loading,
 });
 
 export default rootReducer;
