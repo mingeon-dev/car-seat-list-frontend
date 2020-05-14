eb235a7dc81927ec6ec1d3537e0fd362cdb37d6b
Mingeon Kim <mingeon.dev@gmail.com>
2020-05-14 17:44:09 +0900

    fix: 미사용 페이지(ProductPage) 제거

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
 import ProductListPage from './pages/ProductListPage';
-import ProductPage from './pages/ProductPage';
 
 const App = () => {
   return (
     <>
       <Route component={ProductListPage} path="/" exact />
-      <Route component={ProductPage} path="/:productId" />
     </>
   );
 };
--- src/pages/ProductPage.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-const ProductPage = ({ match }) => {
-  const { productId } = match.params;
-  return <div>ProductId: {productId}</div>;
-};
-
-export default ProductPage;
